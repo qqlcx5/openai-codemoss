055ea966b2516bd1395eaeb5b2cb0bab200ce009
liaochunxin <liaochunxin@antateam.com>
2025-09-11 16:29:01 +0800

    Update default server port to 8002 in env.example and server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8002;
 
 // 中间件
 app.use(cors());
